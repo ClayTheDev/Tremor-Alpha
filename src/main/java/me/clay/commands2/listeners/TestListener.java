6f28cb324caa1dac0d9fccc446be567e983c18fb
DiSK <GalacticalMagic@gmail.com>
2023-01-24 20:33:50 -0600

    Testing Listeners/Events - Fixed it to work now.

--- src/main/java/me/clay/commands2/listeners/TestListener.java
+++ src/main/java/me/clay/commands2/listeners/TestListener.java
@@ -1,15 +1,15 @@
 package me.clay.commands2.listeners;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
 
 public class TestListener implements Listener {
-    public void PlayerRespawnEvent(PlayerRespawnEvent Player){
-        Player.getRespawnLocation();
-        System.out.println(Player.getRespawnLocation());
-
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent Player){
+        System.out.println(Player.getRespawnLocation() + " is the players respawn location.");
     }
 
 }
